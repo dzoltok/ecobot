b490e91b1ed7821da35929d74387c60e296339f6
David Zoltok <dzoltok@onemedical.com>
2019-11-14 14:53:36 -0800

    Standardize response to pull image and description

--- src/trash-instructions.js
+++ src/trash-instructions.js
@@ -2,8 +2,25 @@
 // const https = require('https')
 const rp = require('request-promise');
 
-const RECOLLECT_URL = 'https://api.recollect.net'
-const RECOLLECT_PATH = '/api/areas/recology-1051/services/waste/pages'
+const RECOLLECT_URL = 'https://api.recollect.net';
+const RECOLLECT_PATH = '/api/areas/recology-1051/services/waste/pages';
+
+const RECOLOGY_IMAGE_URLS = {
+  landfill: 'https://recollect-images.global.ssl.fastly.net/api/image/200/recology.blackbin.svg',
+  recycling: 'https://recollect-images.global.ssl.fastly.net/api/image/200/recology.bluebin.svg',
+  compost: 'https://recollect-images.global.ssl.fastly.net/api/image/200/recology.greenbin.svg',
+  other: 'https://recollect-images.global.ssl.fastly.net/api/image/200/recology.otherbin.svg'
+}
+
+// Required when fetching single items, as response structure is inconsistent otherwise
+const WIDGET_CONFIG = {
+  "theme":"recology",
+  "area":"recology-1051",
+}
+
+function toSentenceCase(str) {
+  return str.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g, c => c.toUpperCase());
+}
 
 const trashInstructions = (req, res) => {
   const suggest = req.body.text;
@@ -17,16 +34,26 @@ const trashInstructions = (req, res) => {
 
     return rp({
       json: true,
+      qs: { widget_config: JSON.stringify(WIDGET_CONFIG) },
       uri: `${RECOLLECT_URL}${RECOLLECT_PATH}/en-US/${bestResult.id}.json`,
     });
   }).then(item => {
-    console.log(item);
+    const destinationBin = item.sections[4].title.toLowerCase();
 
-    const instructionSection = item.sections.find(section => section.index === 1);
+    let description, imageUrl;
 
-    const destination = instructionSection.value
+    if (RECOLOGY_IMAGE_URLS[destinationBin]) {
+      description = toSentenceCase(item.sections[2].rows[0].html);
+      imageUrl = RECOLOGY_IMAGE_URLS[destinationBin];
+    } else {
+      description = `${item.sections[2].rows[0].html} has special instructions, please visit https://www.recology.com/recology-san-francisco/what-bin/`;
+      imageUrl = RECOLOGY_IMAGE_URLS.other;
+    }
 
-    res.send(destination);
+    res.send({
+      description,
+      imageUrl
+    });
   });
 }
 
