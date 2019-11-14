ef0033ea440a1ee10a65365d13b828626792ffb9
David Zoltok <dzoltok@onemedical.com>
2019-11-14 13:55:28 -0800

    Use double-query to return correct bin according to Recology

--- src/trash-instructions.js
+++ src/trash-instructions.js
@@ -1,15 +1,33 @@
 // const express = require('express');
+// const https = require('https')
+const rp = require('request-promise');
+
+const RECOLLECT_URL = 'https://api.recollect.net'
+const RECOLLECT_PATH = '/api/areas/recology-1051/services/waste/pages'
 
 const trashInstructions = (req, res) => {
-  console.info(`processing request for trash instructions`);
-  res.send({status: 'ok'});
-  // Parse the req to figure out the search text
+  const suggest = req.body.text;
 
-  // Query the Recology/Recollect API to get the item ID
+  rp({
+    json: true,
+    qs: { suggest },
+    uri: `${RECOLLECT_URL}${RECOLLECT_PATH}`,
+  }).then(results => {
+    const bestResult = results.reduce((prev, current) => parseInt(current.score) > parseInt(prev.score) ? current : prev);
 
-  // Query the API by item ID to get the full payload
+    return rp({
+      json: true,
+      uri: `${RECOLLECT_URL}${RECOLLECT_PATH}/en-US/${bestResult.id}.json`,
+    });
+  }).then(item => {
+    console.log(item);
 
-  //
+    const instructionSection = item.sections.find(section => section.index === 1);
+
+    const destination = instructionSection.value
+
+    res.send(destination);
+  });
 }
 
 module.exports = trashInstructions;
