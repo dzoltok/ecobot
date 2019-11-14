885b1fc10c79add40a7ee40bc6e425edd2fce43d
Umer Khan <ukhan@onemedical.com>
2019-11-14 15:29:41 -0800

    add bin images

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6500;
 
-const app = require('./app');
+const app = require("./app");
 
-const server = app.listen(PORT, (error) => {
+const server = app.listen(PORT, error => {
   if (error) {
     return console.error(`Error starting server: ${error}`);
   }
