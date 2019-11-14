744de354df1e89d95cfa99002ec33bd9cc92031a
David Zoltok <dzoltok@onemedical.com>
2019-11-14 15:09:12 -0800

    Host public sources out of /public

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const trashInstructions = require('./trash-instructions');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.post('/trash-instructions', trashInstructions);
 
