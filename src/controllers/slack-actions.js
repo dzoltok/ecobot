a837e1176c97e7cb74f5a318db5836c741ab053e
David Zoltok <dzoltok@onemedical.com>
2019-11-15 15:37:24 -0800

    Try explicitly parsing the results

--- src/controllers/slack-actions.js
+++ src/controllers/slack-actions.js
@@ -93,9 +93,9 @@ async function dispatchAction(action, payload) {
  * @returns {Promise<Response | void>}
  */
 async function slackActionsController(req, res) {
-  const payload = req.body.payload;
-  console.log(payload['actions']);
-  const action = payload['actions'][0].value;
+  const payload = JSON.parse(req.body.payload);
+  console.log(payload);
+  const action = payload.actions[0].value;
 
   return dispatchAction(action, payload)
     .then(data => res.send({ data }))
